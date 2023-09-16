116af90ddb867a748209db006b4e1adf66254980
Zahin <zahin.kabir@cgs.act.edu.au>
2023-09-16 19:55:46 +1000

    using piezo speaker

--- sketch_sep8b/sketch_sep8b.ino
+++ sketch_sep8b/sketch_sep8b.ino
@@ -1,18 +1,26 @@
 #include <Arduino.h>
 #include <WiFi.h>
 #include <ESPAsyncWebSrv.h>
+#include <driver/ledc.h>
 
 // Constants won't change
-#define LIGHT_SENSOR_PIN  35  // ESP32 pin GPIO35 (ADC1) connected to the light sensor
-#define LED_PIN           27  // ESP32 pin GPIO27 connected to the LED
-#define ANALOG_THRESHOLD  3000 // Threshold value for the light sensor
+#define LIGHT_SENSOR_PIN 35    // ESP32 pin GPIO35 (ADC1) connected to the light sensor
+#define LED_PIN 27             // ESP32 pin GPIO27 connected to the LED
+#define ANALOG_THRESHOLD 3000  // Threshold value for the light sensor
+#define PIEZO_BUZZER_PIN 26    // GPIO for piezo buzzer
 
 // WiFi credentials
-const char* ssid = "Proxima";
-const char* password = "centauri";
+const char* ssid = "TKZ-10";
+const char* password = "Careful11";
 
 // Create an instance of the web server
 AsyncWebServer server(80);
+
+// Define song-related constants and variables
+const int songLength = 18;
+char notes[] = "cdfda ag cdfdg gf ";
+int beats[] = { 1, 1, 1, 1, 1, 1, 4, 4, 2, 1, 1, 1, 1, 1, 1, 4, 4, 2 };
+int tempo = 113;
 
 void setup() {
   // Initialize serial communication
@@ -24,8 +32,16 @@ void setup() {
   // Initialize the LED pin as an output
   pinMode(LED_PIN, OUTPUT);
 
+  // Initialize the piezo buzzer pin as an output
+  pinMode(PIEZO_BUZZER_PIN, OUTPUT);
+
+  // Initialize LEDC timer and channel for the piezo buzzer
+  ledcSetup(0, 1000, 8); // Initialize LEDC channel 0 with a frequency of 1000 Hz and 8-bit depth
+  ledcAttachPin(PIEZO_BUZZER_PIN, 0); // Attach the channel to the piezo buzzer pin
+
+
   // Route for serving sensor data
-  server.on("/data", HTTP_GET, [](AsyncWebServerRequest *request){
+  server.on("/data", HTTP_GET, [](AsyncWebServerRequest* request) {
     // Read the analog value from the light sensor
     int analogValue = analogRead(LIGHT_SENSOR_PIN);
 
@@ -40,28 +56,40 @@ void setup() {
   server.begin();
 }
 
+// Play a sound when the LED turns on or off
+void playBuzzer(int frequency, int duration) {
+  tone(PIEZO_BUZZER_PIN, frequency, duration);
+  delay(duration);           // Optional delay to control the note duration
+  noTone(PIEZO_BUZZER_PIN);  // Turn off the buzzer
+}
+
 void loop() {
   // Read the analog value from the light sensor
   int analogValue = analogRead(LIGHT_SENSOR_PIN);
 
-  // Call a function to control the LED based on the analog value
+  // Call a function to control the LED and play the song based on the analog value
   controlLED(analogValue);
 
   // Read the analog value from the light sensor after updating the LED
   debug();
 }
-
 void controlLED(int analogValue) {
   // Update the LED based on the analog value and threshold
-  if (analogValue < ANALOG_THRESHOLD)
+  if (analogValue < ANALOG_THRESHOLD) {
     digitalWrite(LED_PIN, HIGH); // Turn on the LED if the analog value is below the threshold
-  else
+    playSong(); // Play the song when the LED turns on
+    ledcDetachPin(PIEZO_BUZZER_PIN); // Deinitialize LEDC channel
+  } else {
     digitalWrite(LED_PIN, LOW);  // Turn off the LED if the analog value is above or equal to the threshold
+    noTone(PIEZO_BUZZER_PIN); // Stop the buzzer when the LED turns off
+    ledcDetachPin(PIEZO_BUZZER_PIN); // Deinitialize LEDC channel
+  }
 }
 
-void debug(){
+
+void debug() {
   int analogValue = analogRead(LIGHT_SENSOR_PIN);
-    // Print the analog value to the serial monitor
+  // Print the analog value to the serial monitor
   Serial.print("Light Sensor Value: ");
   Serial.println(analogValue);
   delay(3000);
@@ -85,3 +113,31 @@ void connectToWiFi() {
   Serial.println(WiFi.localIP());
 }
 
+// Function to play the song
+void playSong() {
+  int i, duration;
+  for (i = 0; i < songLength; i++) {
+    duration = beats[i] * tempo;
+    if (notes[i] == ' ') {
+      delay(duration);
+    } else {
+      tone(PIEZO_BUZZER_PIN, frequency(notes[i]), duration);
+      delay(duration);
+    }
+    delay(tempo / 10);
+  }
+}
+
+// Function to get the frequency of a note
+int frequency(char note) {
+  const int numNotes = 8;
+  char names[] = { 'c', 'd', 'e', 'f', 'g', 'a', 'b', 'C' };
+  int frequencies[] = { 262, 294, 330, 349, 392, 440, 494, 523 };
+
+  for (int i = 0; i < numNotes; i++) {
+    if (names[i] == note) {
+      return frequencies[i];
+    }
+  }
+  return 0;  // Return 0 if the note is not found
+}
